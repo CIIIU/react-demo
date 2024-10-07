b88c325af48fc881425f3193d0f3c008977d9cb5
Duong Quoc Hung <quochung11052003@gmail.com>
2024-10-07 16:05:44 +0700

    first comment

---
+++ src/components/users/user.table.tsx
@@ -0,0 +1,176 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useEffect, useState } from 'react';
+// import '../../styles/users.css';
+import { Table, Button, notification,Popconfirm, message } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+
+import { PlusOutlined,EditOutlined,DeleteFilled } from '@ant-design/icons';
+import CreateUserModal from './user.create';
+import UpdateUserModal from './user.update';
+
+export interface IUsers {
+  _id: string;
+  email: string;
+  name: string;
+  role: string;
+  address: string;
+  gender: string;
+  password: string;
+  age: string;
+}
+
+
+const UsersTable = () => {
+
+    const [listUsers, setListUsers] = useState([]);
+
+    const [isCreateModalOpen, setIsCreateModalOpen] = useState(false);
+
+    const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
+
+    const [dataUpdate, setDataUpdate] = useState<null | IUsers>(null);
+
+    const access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ0b2tlbiBsb2dpbiIsImlzcyI6ImZyb20gc2VydmVyIiwiX2lkIjoiNjZmYmE2Yzc1NDFmYWUxMjNiNjMxMGNiIiwiZW1haWwiOiJhZG1pbkBnbWFpbC5jb20iLCJhZGRyZXNzIjoiVmlldE5hbSIsImlzVmVyaWZ5Ijp0cnVlLCJuYW1lIjoiSSdtIGFkbWluIiwidHlwZSI6IlNZU1RFTSIsInJvbGUiOiJBRE1JTiIsImdlbmRlciI6Ik1BTEUiLCJhZ2UiOjY5LCJpYXQiOjE3Mjc5MjY5NTQsImV4cCI6MTgxNDMyNjk1NH0.IjAPCdRcvLmwmijCDYGKksTPW-nDo6uLHKEIPcF-Heg"
+
+    useEffect(() => {
+        //update
+        console.log(">>> check useEffect")
+        getData()
+    }, [])
+
+    const getData = async () => {
+
+        const res = await fetch(
+            "http://localhost:8000/api/v1/users/all",
+            {
+                headers: {
+                    'Authorization': `Bearer ${access_token}`,
+                    "Content-Type": "application/json",
+                },
+            })
+
+        const d = await res.json();
+        if (!d.data) {
+          notification.error({
+              message: JSON.stringify(d.message)
+          })
+      }
+
+        setListUsers(d.data.result)
+    }
+
+    const confirm = async (user: IUsers) => {
+      const res = await fetch(
+          `http://localhost:8000/api/v1/users/${user._id}`,
+          {
+              method: "DELETE",
+              headers: {
+                  'Authorization': `Bearer ${access_token}`,
+                  "Content-Type": "application/json",
+              },
+          })
+
+      const d = await res.json();
+      if (d.data) {
+          notification.success({
+              message: "Xóa user thành công."
+          })
+          await getData();
+      } else {
+          notification.error({
+              message: JSON.stringify(d.message)
+          })
+      }
+  };
+
+
+    const columns: ColumnsType<IUsers> = [
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            render: (value, record) => {
+                return (<div>{record.email}</div>)
+            }
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+        },
+        {
+            title: 'Role',
+            dataIndex: 'role',
+        },
+        {
+            title: 'Actions',
+            render: (value, record) => {
+
+              return (
+                <div>
+                    <Button
+                     onClick={() => {
+                        setDataUpdate(record);
+                        setIsUpdateModalOpen(true)
+                    }}>Edit</Button>
+
+                    <Popconfirm
+                        title="Delete the user"
+                        description={`Are you sure to delete user ${record.name}?`}
+                        onConfirm={() => confirm(record)}
+                        okText="Yes"
+                        cancelText="No"
+                    >
+                        <Button
+                            style={{ marginLeft: 20 }}
+                            danger>
+                            Delete
+                        </Button>
+                    </Popconfirm>
+                </div>)
+
+            }
+        }
+    ]
+
+    return (
+        <div>
+            <div style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center"
+            }}>
+                <h2>Table Users</h2>
+                <div>
+                    <Button
+                        icon={<PlusOutlined />}
+                        type={"primary"}
+                        onClick={() => setIsCreateModalOpen(true)}
+                    >Add new</Button>
+                </div>
+            </div>
+
+            <Table
+                columns={columns}
+                dataSource={listUsers}
+                rowKey={"_id"}
+            />
+
+            <CreateUserModal
+                access_token={access_token}
+                getData={getData}
+                isCreateModalOpen={isCreateModalOpen}
+                setIsCreateModalOpen={setIsCreateModalOpen}
+            />
+
+            <UpdateUserModal
+                access_token={access_token}
+                getData={getData}
+                isUpdateModalOpen={isUpdateModalOpen}
+                setIsUpdateModalOpen={setIsUpdateModalOpen}
+                dataUpdate={dataUpdate}
+                setDataUpdate={setDataUpdate}
+            />
+        </div>
+    )
+}
+
+export default UsersTable;
